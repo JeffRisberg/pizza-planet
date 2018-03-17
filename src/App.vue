2019f80fb9433cec54289501d9df91dc07d6abff
JeffRisberg <jrisberg@aol.com>
2018-03-17 13:29:19 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <ppHeader></ppHeader>
+    </div>
+    <div class="container">
+      <ppHome></ppHome>
+    </div>
+    <div class="container">
+      <ppFooter></ppFooter>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+  import Home from './components/Home.vue'
+
+  export default {
+    components: {
+      ppHeader: Header,
+      ppFooter: Footer,
+      ppHome: Home
+    }
+  }
+</script>
+
+<style>
+
+</style>
